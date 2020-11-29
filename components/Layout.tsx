9f17794b209be92670efe76d4fa4ac691ce3f48f
nlemast <nlemaster47@gmail.com>
2020-11-29 12:18:36 -0500

    Create Layout component to wrap around Header and children

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,40 +1,25 @@
-import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledReset = styled.body`
-  box-sizing: border-box;
-  margin: 0 !important;
-`;
+import CSSReset from './CSSReset';
+import Header from './Header';
 
 const StyledLayout = styled.main`
   background-color: #000;
+  color: #F8D736;
+  font-family: 'Permanent Marker', cursive;
   height: 100vh;
   width: 100vw;
 `;
 
-const StyledImage = styled.section`
-  align-items: center;
-  display: flex;
-  height: 100%;
-  justify-content: center;
-  width: 100%;
-`;
-
-const Layout: React.FC = () => {
+const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <StyledReset>
-        <StyledLayout>
-          <StyledImage>
-            <Image
-              height={750}
-              src="/images/Dominyc.png"
-              width={1000}
-            />
-          </StyledImage>
-        </StyledLayout>
-      </StyledReset>
+      <CSSReset />
+      <StyledLayout>
+        <Header />
+        {children}
+      </StyledLayout>
     </>
   );
 };
