ed2ca4d3b5211c0277195fd18d62d7c9d1e10a73
nlemast <nlemaster47@gmail.com>
2020-11-29 12:17:49 -0500

    Style center logo on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
+import Image from 'next/image';
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
+
+const StyledImage = styled.section`
+  align-items: center;
+  display: flex !important;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`;
 
 const HomePage: React.FC = () => {
   return (
     <Layout>
-      <div>Home Page</div>
+      <StyledImage>
+        <Image
+          height={750}
+          src="/images/Dominyc.png"
+          width={1000}
+        />
+      </StyledImage>
     </Layout>
   );
 };
